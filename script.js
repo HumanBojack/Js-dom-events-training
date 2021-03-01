3e14c633b1e5fb3888f86670094c68491fbcdd83
HumanBojack <spychalar59@gmail.com>
2021-03-01 11:47:46 +0100

    First four things

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-console.log("cul");
+// 1/1.1 Footer clicks counter
+let footer = document.querySelector("footer");
+let clickCount = 0
+function onFooterClick(){
+	clickCount += 1
+	console.log(`Clic n° ${clickCount}`);
+};
+
+footer.addEventListener("click", onFooterClick);
+
+// 2 Hamburger menu
+let buttonToggler = document.querySelector(".navbar-toggler");
+let navbar = document.getElementById("navbarHeader");
+let navbarDeployed = false;
+function navbarExpand() {
+	if (navbarDeployed === true){
+		navbar.classList.add("collapse");
+		navbarDeployed = false;
+	} else if (navbarDeployed === false){
+		navbar.classList.remove("collapse");
+		navbarDeployed = true;
+	}
+}
+buttonToggler.addEventListener("click", navbarExpand);
+
+// 3 Edit on first card
+let firstCard = document.getElementsByClassName("card")[0];
+let firstCardButton = firstCard.getElementsByClassName("btn-group")[0].children[1];
+function onEditButtonClick1(){
+	firstCard.style = "color: red;"
+}
+firstCardButton.addEventListener("click", onEditButtonClick1)
+
+// 4 Edit on second card
+let secondCard = document.getElementsByClassName("card")[1];
+let secondCardButton = secondCard.getElementsByClassName("btn-group")[0].children[1];
+let clicked = false;
+function onEditButtonClick2(){
+	if (clicked === false){
+		secondCard.style = "color: green;";
+		clicked = true;
+	} else {
+		secondCard.style = "color: ;";
+		clicked = false;
+	}
+}
+secondCardButton.addEventListener("click", onEditButtonClick2)
+
+// 
